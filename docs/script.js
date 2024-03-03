6c0ff2cef09519b179f8ad94b26307b4ba827f51
Wendel <wendellduos@hotmail.com>
2024-03-03 20:18:36 -0300

    added more homepage cards

--- docs/script.js
+++ docs/script.js
@@ -6,3 +6,18 @@ document.getElementById("donate-btn").addEventListener("mouseover", () => {
     document.getElementById("heart-pulse").style.animationPlayState = "paused";
   });
 });
+
+// pull info card up on .note-card-alt hover
+const altCardList = document.querySelectorAll(".note-card-alt");
+
+altCardList.forEach((card) => {
+  card.addEventListener("mouseover", () => {
+    card.children[0].style.top = 0;
+    card.children[0].style.backdropFilter = "blur(2px)";
+
+    card.addEventListener("mouseout", () => {
+      card.children[0].style.top = "75%";
+      card.children[0].style.backdropFilter = "blur(0px)";
+    });
+  });
+});
