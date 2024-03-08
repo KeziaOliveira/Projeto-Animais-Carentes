acb976716e4fa100bbac2392b4404259ac18da53
Wendel <wendellduos@hotmail.com>
2024-03-08 15:50:32 -0300

    updated cards section to fit content

--- data/pets.js
+++ data/pets.js
@@ -5,90 +5,105 @@ let pets = {
       image: "amy",
       description:
         "Essa orelhudinha linda é a AMY. Está conosco desde 2020, foi resgatada por uma voluntária, é uma jovem senhora ativa e super carinhosa, mas que infelizmente ainda não foi adotada. Enquanto isso o abrigo é a casa dela.",
+      isMale: false,
     },
     {
       name: "Viny",
       image: "viny",
       description:
         "Esse mocinho lindo é o VINY. Está conosco desde 2020, foi resgatado na rua por uma voluntária, é um grandão lindo e amoroso.",
+      isMale: true,
     },
     {
       name: "Rebeca",
       image: "rebeca",
       description:
         "Essa gracinha é a REBECA. Está conosco desde 2019, foi resgatada na rua cheia de filhotes e muito doente, todos foram adotados, mas ela ainda não teve a chance de ter uma família.",
+      isMale: false,
     },
     {
       name: "Pepo",
       image: "pepo",
       description:
         "Esse grandão é o PEPO. Está conosco desde 2019, foi resgatado de um acumulador e ainda não teve a chance de ter uma família.",
+      isMale: true,
     },
     {
       name: "Léo",
       image: "leo",
       description:
         "Esse senhorzinho é o LÉO. Está conosco desde 2019, foi resgatado por nossas voluntárias, hoje já é um jovem senhor super amoroso e dócil.",
+      isMale: true,
     },
     {
       name: "Oreo",
       image: "oreo",
       description:
         "Esse pequenino é o OREO. Está conosco desde 2019, foi resgatado de um acumulador, sofreu maus tratos e até hoje ainda tem desconfiança com ser humano.",
+      isMale: true,
     },
     {
       name: "Magali",
       image: "magali",
       description:
         "Essa senhorinha é a MAGALI. Está conosco desde 2019, tem seus últimos 3 dentinhos na boca, esbanja simpatia e alegria, mas até hoje não teve a chance de encontrar uma família.",
+      isMale: false,
     },
     {
       name: "Bruna",
       image: "bruna",
       description:
         "Essa linda é a BRUNA. Está conosco desde 2019 e até hoje ela não teve a chance de encontrar uma família.",
+      isMale: false,
     },
     {
       name: "Bily",
       image: "bily",
       description:
         "Esse bebezão é o BILY. Está conosco desde 2019, foi resgatado ainda filhote com seus irmãozinhos, todos foram adotados, mas até hoje ele não teve a chance de encontrar uma família.",
+      isMale: true,
     },
     {
       name: "Tigresa",
       image: "tigresa",
       description:
         "Essa princesa é a TIGRESA. Está conosco desde 2019, foi resgatada de uma acumuladora.",
+      isMale: false,
     },
     {
       name: "Dumbo",
       image: "dumbo",
       description:
         "Esse lindo é o DUMBO. Está conosco desde 2019, chegou magrinho, com TVT (um tipo de câncer) e após muitos cuidados está curado.",
+      isMale: true,
     },
     {
       name: "Júnior",
       image: "junior",
       description:
         "Esse é o príncipe JÚNIOR. Está conosco desde 2018, chegou ainda filhotinho e nunca teve a chance de um lar.",
+      isMale: true,
     },
     {
       name: "Bolinha",
       image: "bolinha",
       description:
         "Essa é a nossa linda BOLINHA. Está conosco desde 2018, passou por muitas coisas e continua aguardando por um lar.",
+      isMale: false,
     },
     {
       name: "Lola",
       image: "lola",
       description:
         "Essa é a nossa princesa LOLA. Está conosco desde 2017, os pelinhos brancos já estão aparecendo e continua infelizmente ainda não foi adotada",
+      isMale: false,
     },
     {
       name: "Vitório",
       image: "vitorio",
       description:
         "Esse é o nosso bebezão VITÓRIO. Está conosco desde 2017 e continua em busca de um lar.",
+      isMale: true,
     },
   ],
   cats: [
@@ -97,16 +112,19 @@ let pets = {
       image: "mileido",
       description:
         "Esse príncipe é o Mileido, machinho. É carinhoso, gosta de colo, adora um carinho na cabeça. Está castrado e vacinado.",
+      isMale: true,
     },
     {
       name: "Everest",
       image: "everest",
       description: "Esse é o EVEREST, macho, castrado e vacinado.",
+      isMale: true,
     },
     {
       name: "Coringa",
       image: "coringa",
       description: "Esse é o CORINGA, macho, castrado e vacinado.",
+      isMale: true,
     },
   ],
 };
